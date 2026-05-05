07a462ca8545497f78596fc2f80f4af3486093b0
rubithedev <rubithedev@gmail.com>
2026-05-05 22:18:21 +0100

    fix: corrected CSP typo

--- src/routes.py
+++ src/routes.py
@@ -113,9 +113,9 @@ app = Flask(__name__)
 def set_security_headers(response):
     response.headers['Content-Security-Policy'] = (
         "default-src 'self' https://jsme-editor.github.io; "
-        "script-src 'sel' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://jsme-editor.github.io https://unpkg.com https://3dmol.org; "
+        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://jsme-editor.github.io https://unpkg.com https://3dmol.org; "
         "worker-src 'self' blob:; "
-        "style-src 'sel' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net https://jsme-editor.github.io; "
+        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net https://jsme-editor.github.io; "
         "font-src 'self' data: https://fonts.gstatic.com https://cdn.jsdelivr.net; "
         "img-src 'self' data: blob: https://jsme-editor.github.io https://cdn.rcsb.org; "
         "connect-src 'self' https://jsme-editor.github.io https://unpkg.com https://files.rcsb.org; "
